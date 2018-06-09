0b7081cb7663c52527936a081aef6500bd292415
noboru-i <ishikura.noboru@gmail.com>
2018-06-09 14:56:01 +0900

    use axios

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,3 +1,23 @@
 <template>
-  <h1>Hello world!</h1>
+  <div>
+    <h1>共円</h1>
+    <div>
+      <h2>最新の登録</h2>
+      <ul id="example-1">
+        <li v-for="stage in stages" :key="stage.id">
+          STAGE:{{ stage.stageNo }} - {{ stage.registDate }}
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  async asyncData({ params, app}) {
+    const stages = await app.$axios.$get('http://my-android-server.appspot.com/api/recent_stages')
+
+    return {stages}
+  }
+}
+</script>
