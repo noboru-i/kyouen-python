5088e986f764b008c15f157812bb2211257fec32
noboru-i <ishikura.noboru@gmail.com>
2018-06-25 00:51:02 +0900

    impl stone event

--- frontend/components/Kyouen.vue
+++ frontend/components/Kyouen.vue
@@ -9,29 +9,64 @@
           v-for="index in Array.from({length: 6}, (v, k) => k)"/>
     </svg>
     <v-btn fab
-        color="blue-grey darken-3"
-        :class="{hide: stone == '0'}"
+        color="blue-grey"
+        :class="{hide: stone == '0', 'darken-3': stone == '1', 'lighten-5': stone == '2'}"
         v-for="(stone, index) in stageArray"
-        :key="index" />
+        :key="index"
+        @click="onClick(index)" />
   </div>
 </template>
 
 <script>
 import { mapState } from 'vuex'
 
+const boardSize = 6;
+
+String.prototype.replaceCharAt = function(at, replaceChar) {
+  const before = this.substring(0, at);
+  const after = this.slice(at + 1);
+  return before + replaceChar + after;
+};
+
+class TumeKyouen {
+  constructor(stage) {
+    this.stage = stage;
+  }
+
+  selecWithIndext(index) {
+    const c = this.stage.charAt(index);
+    this.stage = this.stage.replaceCharAt(index, this.getNextChar(c));
+  }
+
+  getNextChar(char) {
+    if (char === '1') {
+      return '2';
+    } else if (char === '2') {
+      return '1';
+    }
+    return char;
+  }
+}
+
 export default {
   data: function() {
     return {
-      width: 336
+      width: 336,
+      kyouen: new TumeKyouen(this.stage)
     }
   },
   props: ['stage'],
   computed: {
     stoneSize: function() {
-      return this.width / 6;
+      return this.width / boardSize;
     },
     stageArray: function() {
-      return this.stage.split('');
+      return this.kyouen.stage.split('');
+    }
+  },
+  methods: {
+    onClick: function (index) {
+      this.kyouen.selecWithIndext(index);
     }
   }
 }
