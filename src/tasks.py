7b8d504141631f2c9c088ab7624a6b35c826271b
noboru-i <ishikura.noboru@gmail.com>
2019-02-01 21:54:11 +0900

    update FCM

--- src/tasks.py
+++ src/tasks.py
@@ -10,10 +10,6 @@ from google.appengine.ext import ndb
 from models import RegistModel, GcmModel, ApnsModel
 from const import Const
 
-# https://github.com/djacobs/PyAPNs/blob/master/apns.py
-from libs.pyapns.apns import APNs, Payload, PayloadAlert
-
-
 # twitterに投稿
 def post_twitter(message):
     auth = tweepy.OAuthHandler(Const.TWITTER_SHARE_CONSUMER_KEY, Const.TWITTER_SHARE_CONSUMER_SECRET)
@@ -22,74 +18,35 @@ def post_twitter(message):
     api.update_status(message)
     return
 
-
-def sendGcm(gcmModel):
-    from google.appengine.api import urlfetch
-    import urllib
-
-    form_fields = {
-                   "registration_id": gcmModel.registrationId,
-                   "collapse_key": "update",
-                   "data.message": "gcm message!!!"
-    }
-    form_data = urllib.urlencode(form_fields)
-    result = urlfetch.fetch(url='https://android.googleapis.com/gcm/send',
-                            payload=form_data,
-                            method=urlfetch.POST,
-                            headers={
-                                     'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-                                     'Authorization': 'key=' + Const.GCM_API_KEY
-                                     })
-    logging.debug('regId=' + gcmModel.registrationId + ', statusCode=' + str(result.status_code))
-    return result
-
-
-def sendGcmAll():
-    query = GcmModel.query()
-    for m in query:
-        sendGcm(m)
-
-    return True
-
 def sendFcmAll():
     from google.appengine.api import urlfetch
     import urllib
+    from oauth2client.service_account import ServiceAccountCredentials
+
+    SCOPES = ['https://www.googleapis.com/auth/firebase.messaging']
+
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(
+        'certificate/api-project-1046368181881-firebase-adminsdk-df1u6-8141bfec61.json', SCOPES)
+    access_token_info = credentials.get_access_token()
 
     form_fields = {
-                   'to': '/topics/all',
-                   "data.message": "fcm message!!!"
+        'message': {
+            'topic': 'all',
+            'notification': {
+                'title': '新規ステージが追加されました。'
+            }
+        }
     }
     form_data = json.dumps(form_fields)
-    result = urlfetch.fetch(url='https://fcm.googleapis.com/fcm/send',
+    result = urlfetch.fetch(url='https://fcm.googleapis.com/v1/projects/api-project-1046368181881/messages:send',
                             payload=form_data,
                             method=urlfetch.POST,
                             headers={
-                                     'Content-Type': 'application/json',
-                                     'Authorization': 'key=' + Const.FCM_API_KEY
+                                     'Content-Type': 'application/json; UTF-8',
+                                     'Authorization': 'Bearer ' + access_token_info.access_token
                                      })
     logging.debug('content=' + str(result.content) + ', statusCode=' + str(result.status_code))
     return result
-
-def sendApns(apnsModel):
-    apns = APNs(use_sandbox=False, cert_file='certificate/aps_production.pem')  # 本番
-    #apns = APNs(use_sandbox=True, cert_file='certificate/aps_development.pem') # サンドボックス
-    token_hex = apnsModel.deviceToken
-
-    loc_key = 'notification_new_stage'
-    badge = 1
-    alert = PayloadAlert(None, loc_key=loc_key)
-    payload = Payload(alert=alert, badge=badge)
-
-    apns.gateway_server.send_notification(token_hex, payload)
-    logging.debug('deviceToken=' + apnsModel.deviceToken)
-
-
-def sendApnsAll():
-    query = ApnsModel.query()
-    for m in query:
-        sendApns(m)
-    return True
-
 
 class TweetTask(webapp2.RequestHandler):
     def get(self):
@@ -130,12 +87,8 @@ class TweetTask(webapp2.RequestHandler):
         query = RegistModel.query().order(RegistModel.registDate)
         ndb.delete_multi([m.key for m in query])
 
-        # GCMで送信
-        sendGcmAll()
         # FCMで送信
         sendFcmAll()
-        # APNSで送信
-        # sendApnsAll()
         return
 
 application = webapp2.WSGIApplication([('/tasks/tweet', TweetTask),
